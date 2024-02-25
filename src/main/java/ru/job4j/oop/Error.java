a181c0d46abedc78b63c6f487b3e1aa9db43f107
MatveyRo <obzor.matvey@gmail.com>
2024-02-25 13:56:00 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,29 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printError() {
+        System.out.println("Active: " + active);
+        System.out.println("Status: " + status);
+        System.out.println("Message: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error errorObvious = new Error();
+        Error error = new Error(true, 100, "Error 404");
+        errorObvious.printError();
+        error.printError();
+    }
+}
