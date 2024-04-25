091806b3df145788b716191d8de89682f7ce5d58
MatveyRo <obzor.matvey@gmail.com>
2024-04-25 23:11:19 +0300

    4. Принципы раннего возврата и охранных выражений [#504875 #492644]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -21,20 +21,21 @@ public class PasswordValidator {
         boolean hasDigit = false;
         boolean hasSpecial = false;
 
-        while (!hasUpCase && !hasLowCase && !hasDigit && !hasSpecial) {
-            for (char symbol : password.toCharArray()) {
-                if (Character.isUpperCase(symbol)) {
-                    hasUpCase = true;
-                }
-                if (Character.isLowerCase(symbol)) {
-                    hasLowCase = true;
-                }
-                if (Character.isDigit(symbol)) {
-                    hasDigit = true;
-                }
-                if (!Character.isLetterOrDigit(symbol)) {
-                    hasSpecial = true;
-                }
+        for (char symbol : password.toCharArray()) {
+            if (Character.isUpperCase(symbol)) {
+                hasUpCase = true;
+            }
+            if (Character.isLowerCase(symbol)) {
+                hasLowCase = true;
+            }
+            if (Character.isDigit(symbol)) {
+                hasDigit = true;
+            }
+            if (!Character.isLetterOrDigit(symbol)) {
+                hasSpecial = true;
+            }
+            if (hasUpCase && hasLowCase && hasDigit && hasSpecial) {
+                break;
             }
         }
         if (!hasUpCase) {
